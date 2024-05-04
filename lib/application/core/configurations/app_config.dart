3f9798ea50d512e0cc188c13268a0ad732702fad
Joel <joel_f@epiclanka.net>
2024-05-04 19:34:39 +0530

    comments

--- lib/application/core/configurations/app_config.dart
+++ lib/application/core/configurations/app_config.dart
@@ -3,10 +3,6 @@
 import '../utils/app_enums.dart';
 
 class AppConfig {
-  //TODO Releases - HUAWEI : {sourceVerification = false}
-  //TODO Disable Screenshot take
-  //TODO Change Session Time out 2 * 60
-  //TODO Change Release Version
   static DeviceOS deviceOS = DeviceOS.android;
 
   ///Security Config
